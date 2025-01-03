a58ef20496b02206664aa8286e536f796c8e5971
Suraj Patel <surajpatelsuraj55@gmail.com>
2025-01-03 21:30:14 +0530

    feat:every possible moves shows a dot

--- client/src/components/ChessBoard.jsx
+++ client/src/components/ChessBoard.jsx
@@ -1,14 +1,19 @@
 import { Chessboard } from 'react-chessboard'
 import { Box } from '@chakra-ui/react'
 
-function ChessBoard({ onPieceDrop, position }) {
+function ChessBoard({ onPieceDrop, position, onPieceClick, customSquareStyles }) {
   return (
     <Box width="100%" maxW="600px" mx="auto">
       <Chessboard 
         position={position}
         onPieceDrop={onPieceDrop}
+        onSquareClick={onPieceClick}
+        onPieceClick={onPieceClick}
+        customSquareStyles={customSquareStyles}
         boardWidth={600}
         animationDuration={200}
+        areArrowsAllowed={false}
+        showBoardNotation={true}
       />
     </Box>
   )
