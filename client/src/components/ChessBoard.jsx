9596a2e73edbb2c548ff1b095cd3708220d7f7a5
Suraj Patel <surajpatelsuraj55@gmail.com>
2025-01-03 21:55:57 +0530

    style: changed board color

--- client/src/components/ChessBoard.jsx
+++ client/src/components/ChessBoard.jsx
@@ -2,6 +2,14 @@ import { Chessboard } from 'react-chessboard'
 import { Box } from '@chakra-ui/react'
 
 function ChessBoard({ onPieceDrop, position, onPieceClick, customSquareStyles }) {
+  const customBoardStyle = {
+    borderRadius: '4px',
+    boxShadow: '0 2px 10px rgba(0, 0, 0, 0.5)',
+  }
+
+  const customDarkSquareStyle = { backgroundColor: '#5B793D' }  
+  const customLightSquareStyle = { backgroundColor: '#eeeeee' } 
+
   return (
     <Box width="100%" maxW="600px" mx="auto">
       <Chessboard 
@@ -14,6 +22,9 @@ function ChessBoard({ onPieceDrop, position, onPieceClick, customSquareStyles })
         animationDuration={200}
         areArrowsAllowed={false}
         showBoardNotation={true}
+        customBoardStyle={customBoardStyle}
+        customDarkSquareStyle={customDarkSquareStyle}
+        customLightSquareStyle={customLightSquareStyle}
       />
     </Box>
   )
