f59894798bf8a1d4e7d5ca0791a019dda8550fb8
Suraj Patel <surajpatelsuraj55@gmail.com>
2025-02-13 02:05:16 +0530

    test: try5

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { GameModeType } from "../types";
 
 const isDevelopment = import.meta.env.MODE === "development";
-const API_URL = isDevelopment ? "http://localhost:5000" : "https://vercel.com/suraj-patels-projects-a4792e8b/chess-server";
+const API_URL = isDevelopment ? "http://localhost:5000/api/stockfish" : "https://vercel.com/suraj-patels-projects-a4792e8b/chess-server/api/stockfish";
 
 const API = axios.create({
   baseURL: API_URL,
